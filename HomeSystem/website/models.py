836b692e7df27dd8c72642bddf778fa11f5ed318
NathanZK <nathanzelalem100@gmail.com>
2022-05-24 10:23:14 +0300

    database modified

--- HomeSystem/website/models.py
+++ HomeSystem/website/models.py
@@ -2,10 +2,18 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    temp = db.Column(db.Boolean, default=False, nullable=False)
+    humid = db.Column(db.Boolean, default=False, nullable=False)
+    lock = db.Column(db.Boolean, default=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    rooms = db.relationship('Room')
     
--- HomeSystem/website/views.py
+++ HomeSystem/website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from . import db
 import json
+from .models import Room
 #import Adafruit_DHT
 
 
@@ -14,14 +15,32 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("Home.html", user=current_user)
 
-@views.route('/Rooms', methods=['GET'])
+@views.route('/Rooms', methods=['GET', 'POST'])
 @login_required
 def rooms():
+    
+    room = request.form.get('name')
     return render_template("My-rooms.html", user=current_user)
 
-@views.route('/Devices', methods=['GET'])
+@views.route('/Devices', methods=['GET', 'POST'])
 @login_required
 def devices():
-   # humidity, temperature = Adafruit_DHT.read_retry(11,4)
-    return render_template("My-Devices.html", user=current_user )
+    
+    humidity, temperature = Adafruit_DHT.read_retry(11,4)
+    return render_template("My-Devices.html", user=current_user, humid = humidity, temp =temperature )
 
+@views.route('/AddRoom', methods=['GET', 'POST'])
+@login_required
+def addroom():
+    if request.method == 'POST':
+        name = request.form.get('room')
+        temp = request.form['temp']
+        humid = request.form['humid']
+        lock = request.form['lock']
+        
+
+        new_room = Room( name=name, temp = temp, humid = humid, lock =lock, user_id=current_user.id)
+        db.session.add(new_room)
+        db.session.commit()
+        flash('Room added!', category='success')
+    return render_template("addroom.html", user=current_user )
