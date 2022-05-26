e8511f38245466809806d03079eb1ce8b1903003
NathanZK <nathanzelalem100@gmail.com>
2022-05-26 12:49:05 +0300

    Add room functionality Modified

--- HomeSystem/website/views.py
+++ HomeSystem/website/views.py
@@ -21,36 +21,32 @@ def home():
 @login_required
 def rooms():
     
-    
-    new_room = Room(name ="Living Room", temp=True, humid=True , lock = True, user_id=current_user.id)
-    db.session.add(new_room)
-    db.session.commit()
     return render_template("My-rooms.html", user=current_user)
 
 @views.route('/Devices', methods=['GET', 'POST'])
 @login_required
 
 def devices():
-    from gpiozero import Servo
-    from time import sleep
-    myGPIO = 25
-    myCorrection = 0.45
-    maxPW = (2.0 + myCorrection) / 1000
-    minPW = (1.0 - myCorrection) / 1000
-    servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width=maxPW)
+    # from gpiozero import Servo
+    # from time import sleep
+    # myGPIO = 25
+    # myCorrection = 0.45
+    # maxPW = (2.0 + myCorrection) / 1000
+    # minPW = (1.0 - myCorrection) / 1000
+    # servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width=maxPW)
 
 
     str= "Locked"
     if request.method == 'POST':
         if request.form['submit_button'] == 'Lock':
             str= "Locked"
-            servo.min()
-            servo.min()
-            sleep(1)
+            # servo.min()
+            # servo.min()
+            # sleep(1)
         elif request.form['submit_button'] == 'Unlock':
             str="Unlocked"
-            servo.max()
-            sleep(1)
+            # servo.max()
+            # sleep(1)
             
     humidity, temperature = 35, 21 #Adafruit_DHT.read_retry(11,4)
     return render_template("My-Devices.html", user=current_user, humid = humidity, temp =temperature, str=str )
@@ -59,16 +55,21 @@ def devices():
 @login_required
 def addroom():
 
-    
+    if request.method == 'GET':
+        return render_template("addroom.html", user=current_user )
+
     if request.method == 'POST':
-        room = request.form.get('room')
-        temp = request.form.get('temp')
-        humid = request.form.get('humid')
-        lock = request.form.get('lock')
+        room = request.form['room']
+        temp = request.form['temp']
+        humid = request.form['humid']
+        lock = request.form['lock']
         
-        new_room = Room("Living Room", user_id=current_user.id)
+        new_room = Room(name =room, user_id=current_user.id)
         db.session.add(new_room)
         db.session.commit()
+        
+        return render_template("My-rooms.html", user=current_user)
 
+        
        
-    return render_template("addroom.html", user=current_user )
+    
